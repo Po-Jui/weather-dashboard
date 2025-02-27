33de642a0b93927178b7ddf3280272434c759e3a
PoJui Chen <a0970872656@gmail.com>
2025-02-28 07:56:48 +0800

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <v-main>
+      <!-- Date -->
+      <v-container class="text-center">
+        <v-row justify="center">
+          <v-col cols="12">
+            <h1
+              style="
+                font-weight: 900;
+                font-family: 'Courier New', Courier, monospace;
+                margin-bottom: -50px;
+              "
+            >
+              {{
+                new Date().toLocaleDateString("zh-TW", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })
+              }}
+            </h1>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <!-- search -->
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" md="6">
+            <SearchInput @place-data="addPlace" />
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <!-- weather cards -->
+      <v-container>
+        <v-row>
+          <v-col v-for="(place, index) in places" :key="index" cols="12" md="4">
+            <WeatherCard :place="place" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
+import SearchInput from "./components/SearchInput.vue";
+import WeatherCard from "./components/WeatherCard.vue";
+import { ref } from "vue";
 
-nav {
-  padding: 30px;
+const places = ref([]);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+const addPlace = (place) => places.value.unshift(place);
+</script>
